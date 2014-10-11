3ead9ca89dc278ec66bcf56c6bbd2e2206f9ff8a
Jonai Soto <jonailive@gmail.com>
2014-10-11 14:37:29 -0400

    Configured main math functions

--- calculator.rb
+++ calculator.rb
@@ -7,9 +7,58 @@ while (answer != 'yes') and (answer != 'no')
 	answer = gets.chomp.downcase
 end
 
-	if 	answer == 'yes'
-			print "Awesome, lets start!\n"
+if 	answer == 'yes'
+		print "Awesome, lets start!\n"
 
 elsif 	answer == 'no'
 			print "Ok, goodbye.\n"
+			exit 
+end
+
+print "With THE CALCULATOR you can do simple arithmetic. You can add, subtract, multiply, or divide.\n"
+print "Pick two numbers. "
+print "Then pick if you want to add, subtract, multiply, or divide.\n"
+
+print "What is your first number?"
+num1 = gets.chomp.to_i
+
+print "what is your second number?"
+num2 = gets.chomp.to_i
+
+print "Do you want to add, subtract, multiply, or divide?"
+operator = gets.chomp.downcase
+
+
+def add_func (num1, num2) 
+	print num1 + num2
+end
+
+def sub_func (num1, num2)
+	print num1 - num2
+end
+
+def mult_func (num1, num2)
+	print num1 * num2
+end
+
+def div_func (num1, num2)
+	print num1 / num2
+end
+
+add_func = "add"
+sub_func = "subtract"
+mult_func = "multiply"
+div_func = "divide"
+
+
+if operator == "add"
+	add_func(num1, num2)
+elsif operator == "subtract"
+	sub_func(num1, num2)
+elsif operator == "multiply"
+	mult_func(num1, num2)
+elsif operator == "divide"
+	div_func(num1, num2)
+else 
+	print "Must use 'add', 'subtract', 'multiply', or 'divide'!"
 end
