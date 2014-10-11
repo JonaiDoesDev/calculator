b6d80dfbdbbe9116bf16ac860fc1d3b296325645
Jonai Soto <jonailive@gmail.com>
2014-10-11 00:53:11 -0400

    fixed while loop

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-print 'This is a calculator app'
+print 'Hello, welcome to THE CALCULATOR! '
+print "Do you want to use it?\n"
+answer = gets.chomp.downcase
+
+while (answer != 'yes') and (answer != 'no') 
+	print "You must type in \"yes\" or \"no\".\n"
+	answer = gets.chomp.downcase
+end
+
+	if 	answer == 'yes'
+			print "Awesome, lets start!\n"
+
+elsif 	answer == 'no'
+			print "Ok, goodbye.\n"
+end
