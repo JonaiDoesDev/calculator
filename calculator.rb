c8bde9f29900488fdf7e99d3a234b2ce87a3ad9d
Jonai Soto <jonailive@gmail.com>
2015-02-06 10:48:59 -0500

    trying to relearn how to commit

--- calculator.rb
+++ calculator.rb
@@ -3,12 +3,12 @@ print "Do you want to use it?\n"
 answer = gets.chomp.downcase
 
 while (answer != 'yes') and (answer != 'no') 
-	print "You must type in \"yes\" or \"no\".\n"
-	answer = gets.chomp.downcase
+		print "You must type in \"yes\" or \"no\".\n"
+			answer = gets.chomp.downcase
 end
 
-if 	answer == 'yes'
-		print "Awesome, lets start!\n"
+if 		answer == 'yes'
+			print "Awesome, lets start!\n"
 
 elsif 	answer == 'no'
 			print "Ok, goodbye.\n"
@@ -18,14 +18,29 @@ end
 print "With THE CALCULATOR you can do simple arithmetic. You can add, subtract, multiply, or divide.\n"
 print "Pick two numbers. "
 print "Then pick if you want to add, subtract, multiply, or divide.\n"
-
 print "What is your first number?"
-num1 = gets.chomp.to_i
+num1 = gets.chomp.downcase
+
+
+alphabet = Array.new
+alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+while num1 == alphabet
+	print "Must be an integer. Try Again.\n"
+	num1 = gets
+end
+
+if 
+	num1 != alphabet
+	print "Your first number is #{num1} "
+end
+
+
 
 print "what is your second number?"
 num2 = gets.chomp.to_i
 
-print "Do you want to add, subtract, multiply, or divide?"
+print "Do you want to add, subtract, multiply, or divide?\n"
 operator = gets.chomp.downcase
 
 
